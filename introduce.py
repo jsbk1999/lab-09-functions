c32edd50b2e5104f04d6713229dea0e47bd45dd6
Jsbk1999 <jsoriano1499@gmail.com>
2021-04-16 21:37:56 -0400

    finished introduce.py

---
+++ introduce.py
@@ -0,0 +1,7 @@
+def introduce (person1, person2): # function definiton with input PARAMETERS
+    print("Hello " + person1 + " this is " + person2 + ".")
+
+p1 = input("Name a person: ")
+p2 = input("Okay name another person: ")
+
+introduce(p1, p2) # function call at the end of the script with AGRUMENTS
